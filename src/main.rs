c7372c5a1e3567a149051874d3377dd0faf0c19d
Bart Massey <bart.massey@gmail.com>
2024-05-22 13:55:02 -0700

    finished demo

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
+extern crate serde;
+use gloo_net::http;
+extern crate wasm_bindgen_futures;
 use yew::prelude::*;
 
 #[function_component(App)]
 fn app() -> Html {
-    #[derive(Clone, PartialEq)]
+    #[derive(Clone, PartialEq, serde::Deserialize)]
     struct Video {
         id: usize,
         title: String,
@@ -10,32 +13,24 @@ fn app() -> Html {
         url: String,
     }
 
-    let videos = vec![
-        Video {
-            id: 1,
-            title: "Building and breaking things".to_string(),
-            speaker: "John Doe".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-        Video {
-            id: 2,
-            title: "The development process".to_string(),
-            speaker: "Jane Smith".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-        Video {
-            id: 3,
-            title: "The Web 7.0".to_string(),
-            speaker: "Matt Miller".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-        Video {
-            id: 4,
-            title: "Mouseless development".to_string(),
-            speaker: "Tom Jerry".to_string(),
-            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
-        },
-    ];
+    let videos = use_state(|| vec![]);
+    {
+        let videos = videos.clone();
+        use_effect_with((), move |_| {
+            let videos = videos.clone();
+            wasm_bindgen_futures::spawn_local(async move {
+                let fetched_videos: Result<Vec<Video>, gloo_net::Error> =
+                    http::Request::get("/tutorial/data.json")
+                    .send()
+                    .await
+                    .unwrap()
+                    .json()
+                    .await;
+                videos.set(fetched_videos.unwrap());
+            });
+            || ()
+        });
+    }
 
     #[derive(Properties, PartialEq)]
     struct VideosListProps {
@@ -95,7 +90,7 @@ fn app() -> Html {
         <h1>{ "RustConf Explorer" }</h1>
         <div>
             <h3>{"Videos to watch"}</h3>
-            <VideosList videos={videos} on_click={on_video_select.clone()} />
+            <VideosList videos={(*videos).clone()} on_click={on_video_select.clone()} />
         </div>
         { for details }
     </>
