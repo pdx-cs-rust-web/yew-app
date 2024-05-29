67260fb42eac5e2fb750471edbe55515dfafe7d7
Bart Massey <bart.massey@gmail.com>
2024-05-29 09:56:20 -0700

    flattened

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,57 @@ use gloo_net::http;
 extern crate wasm_bindgen_futures;
 use yew::prelude::*;
 
+#[derive(Clone, PartialEq, serde::Deserialize)]
+struct Video {
+    id: usize,
+    title: String,
+    speaker: String,
+    url: String,
+}
+
+#[derive(Properties, PartialEq)]
+struct VideosListProps {
+    videos: Vec<Video>,
+    on_click: Callback<Video>,
+}
+
+#[function_component(VideosList)]
+fn videos_list(VideosListProps { videos, on_click }: &VideosListProps) -> Html {
+    let on_click = on_click.clone();
+    videos.iter().map(|video| {
+        let on_video_select = {
+            let on_click = on_click.clone();
+            let video = video.clone();
+            Callback::from(move |_| {
+                on_click.emit(video.clone())
+            })
+        };
+
+        html! {
+            <p key={video.id} onclick={on_video_select}>{format!("{}: {}", video.speaker, video.title)}</p>
+        }
+    }).collect()
+
+}
+
+#[derive(Properties, PartialEq)]
+struct VideosDetailsProps {
+    video: Video,
+}
+
+#[function_component(VideoDetails)]
+fn video_details(VideosDetailsProps { video }: &VideosDetailsProps) -> Html {
+    html! {
+        <div>
+            <h3>{ video.title.clone() }</h3>
+            <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
+        </div>
+    }
+}
+
+
 #[function_component(App)]
 fn app() -> Html {
-    #[derive(Clone, PartialEq, serde::Deserialize)]
-    struct Video {
-        id: usize,
-        title: String,
-        speaker: String,
-        url: String,
-    }
-
     let videos = use_state(|| vec![]);
     {
         let videos = videos.clone();
@@ -30,46 +71,6 @@ fn app() -> Html {
             });
             || ()
         });
-    }
-
-    #[derive(Properties, PartialEq)]
-    struct VideosListProps {
-        videos: Vec<Video>,
-        on_click: Callback<Video>,
-    }
-
-    #[function_component(VideosList)]
-    fn videos_list(VideosListProps { videos, on_click }: &VideosListProps) -> Html {
-        let on_click = on_click.clone();
-        videos.iter().map(|video| {
-            let on_video_select = {
-                let on_click = on_click.clone();
-                let video = video.clone();
-                Callback::from(move |_| {
-                    on_click.emit(video.clone())
-                })
-            };
-
-            html! {
-                <p key={video.id} onclick={on_video_select}>{format!("{}: {}", video.speaker, video.title)}</p>
-            }
-        }).collect()
-
-    }
-    
-    #[derive(Properties, PartialEq)]
-    struct VideosDetailsProps {
-        video: Video,
-    }
-
-    #[function_component(VideoDetails)]
-    fn video_details(VideosDetailsProps { video }: &VideosDetailsProps) -> Html {
-        html! {
-            <div>
-                <h3>{ video.title.clone() }</h3>
-                <img src="https://via.placeholder.com/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
-            </div>
-        }
     }
 
     let selected_video = use_state(|| None);
